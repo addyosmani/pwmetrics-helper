d35623c5b550b377d2b0698fac167089160da194
Addy Osmani <addyosmani@gmail.com>
2016-10-24 15:59:32 -0700

    Add tests

---
+++ test.js
@@ -0,0 +1,38 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+'use strict';
+
+const getMetrics = require('./');
+const assert = require('assert');
+
+it('should return a valid time-to-interactive score from the report', () => {
+    return getMetrics('https://airhorner.com', {
+        desiredMetric: 'Time to Interactive'
+    }).then(score => {
+        assert(score < 5000);
+    });
+}).timeout(90000);
+
+it('should return a valid first-contentful-paint score from the report', () => {
+    return getMetrics('https://voice-memos.appspot.com', {
+        desiredMetric: 'First Contentful Paint'
+    }).then(score => {
+        assert(score < 5000);
+    });
+}).timeout(90000);
